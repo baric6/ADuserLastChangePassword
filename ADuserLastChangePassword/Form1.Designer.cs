b0d87ee357d7eea8878a04b1680217db6fd27b9f
J.Hollon <baric0748@gmail.com>
2023-07-19 15:52:28 -0400

    Add project files.

---
+++ ADuserLastChangePassword/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace ADuserLastChangePassword
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.output = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.instructionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.DomainName = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(3, 53);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(285, 20);
+            this.txtSearch.TabIndex = 0;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(294, 53);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 1;
+            this.btnSearch.Text = "search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // output
+            // 
+            this.output.Location = new System.Drawing.Point(3, 79);
+            this.output.Name = "output";
+            this.output.Size = new System.Drawing.Size(366, 152);
+            this.output.TabIndex = 2;
+            this.output.Text = "";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(371, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem,
+            this.instructionsToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(35, 20);
+            this.fileToolStripMenuItem.Text = "file";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.exitToolStripMenuItem.Text = "exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // instructionsToolStripMenuItem
+            // 
+            this.instructionsToolStripMenuItem.Name = "instructionsToolStripMenuItem";
+            this.instructionsToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.instructionsToolStripMenuItem.Text = "instructions ";
+            this.instructionsToolStripMenuItem.Click += new System.EventHandler(this.instructionsToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(171, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "SamAccountName / Logon Name ";
+            // 
+            // DomainName
+            // 
+            this.DomainName.AutoSize = true;
+            this.DomainName.Location = new System.Drawing.Point(55, 24);
+            this.DomainName.Name = "DomainName";
+            this.DomainName.Size = new System.Drawing.Size(76, 13);
+            this.DomainName.TabIndex = 5;
+            this.DomainName.Text = "not connected";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(0, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Domain: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(371, 235);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.DomainName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "User Last Login";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.RichTextBox output;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem instructionsToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label DomainName;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ADuserLastChangePassword/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.DirectoryServices.AccountManagement;
+using System.DirectoryServices;
+using System.DirectoryServices.ActiveDirectory;
+
+namespace ADuserLastChangePassword
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            txtSearch.KeyDown += txtSearch_KeyDown;
+
+            Domain domain = Domain.GetCurrentDomain();
+            DomainName.Text = domain.Name;
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            PerformSearch();
+        }
+
+        private void txtSearch_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                // Prevent the Enter key from making a sound by handling the KeyDown event
+                e.SuppressKeyPress = true;
+                PerformSearch();
+            }
+        }
+
+        private void PerformSearch()
+        {
+            try
+            {
+                output.Clear(); // Clear previous search results
+
+                using (PrincipalContext context = new PrincipalContext(ContextType.Domain))
+                {
+                    using (UserPrincipal userPrincipal = new UserPrincipal(context))
+                    {
+                        // Set the SamAccountName and DisplayName properties to search for a partial match
+                        userPrincipal.SamAccountName = "*" + txtSearch.Text + "*";
+
+                        using (PrincipalSearcher searcher = new PrincipalSearcher(userPrincipal))
+                        {
+                            foreach (UserPrincipal user in searcher.FindAll())
+                            {
+                                // Retrieve user information
+                                string fullName = user.DisplayName;
+                                string logonName = user.SamAccountName;
+                                DateTime? lastPasswordChange = user.LastPasswordSet;
+
+                                // Add the user information to the ListBox with the last password change details
+                                output.AppendText($"Full Name: {fullName}, Logon Name: {logonName}, Last Password Change: {lastPasswordChange}\n\n");
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle any exceptions that might occur during the AD query
+                MessageBox.Show($"There is nothing to return", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void instructionsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"Run Program as admin and make sure this computer is connected to a AD \n\n SAMAccountName or Logon Name is what a user signs into computers with like john.deer or j.deer depending on what you are using.\n\n" +
+                $" This program with auto connect to your active directory if you are a admin and search for a users name and will populate when user has last changed thier password." 
+                , "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
